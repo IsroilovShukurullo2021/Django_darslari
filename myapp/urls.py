ec88742c0556214099dabb2a832a3811f8cbd4ba
IsroilovShukurullo2021 <ishbuzcoders@gmail.com>
2021-08-05 10:40:52 +0300

    Django

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from .import views
 urlpatterns =[
-    path('',views.hello),
+    path('',views.hello, name='index'),
+    path('register/',views.register, name='register'),
+    path('login/',views.login, name='login'),
+    path('login/praktika/',views.praktika, name='praktika'),
+    path('logout/',views.logout,name='logout')
 
-    path('maktab/',views.goodbye),
-
-    path('ism/',views.ism),
-
-    path('sahifa/',views.sahifa),
 
 ]
